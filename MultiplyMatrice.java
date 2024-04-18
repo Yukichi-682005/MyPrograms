e7aa6dd30bd47bd9ddb35c3290a5b019e38e676b
Yukichi <bung682005@gmail.com>
2024-04-18 21:23:27 +0530

    one more

---
+++ MultiplyMatrice.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+class MultiplyingMatrice{
+    void GetValue(){
+        int [][] matrice1 = new int [3][3];
+        int [][] matrice2 = new int [3][3];
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                System.out.println("Enter the values of the 1st matrice:");
+                System.out.print("Enter the value in the position "+ i + j);
+                matrice1[i][j] = sc.nextInt();
+            }
+        }
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                System.out.println("Enter the values of the 2st matrice:");
+                System.out.print("Enter the value in the position "+ i + j);
+                matrice2[i][j] = sc.nextInt();
+            }
+        }
+        sc.close();
+
+        Multiplying(matrice1, matrice2);
+    }
+    void Multiplying(int[][] matrice1, int[][] matrice2){
+        int [][] mul = new int [3][3];
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                mul[i][j] = 0;
+                for (int k = 0; k < 3; k++) {
+                    mul[i][j] += matrice1[i][k] * matrice2[k][j];
+                }
+            }
+        }
+        System.out.println("Multiplication of the matrices:");
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                System.out.print(mul[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class MultiplyMatrice {
+    public static void main(String [] args){
+        MultiplyingMatrice obj = new MultiplyingMatrice();
+        obj.GetValue();
+    }
+}
